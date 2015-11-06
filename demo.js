6b5b848313c9a100be00c8dcaeddda60e374c843
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2015-11-06 00:02:52 -0500

    Improve demo.js –with styles– to better show off this module

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
 /*global console, window, document*/
+
 // can be run with `npm run demo`
 var FileDropView = require('./ampersand-file-drop-view');
 var FormView = require('ampersand-form-view');
@@ -25,3 +26,35 @@ formView.render();
 window.formView = formView;
 
 document.body.appendChild(formView.el);
+
+var style = document.createElement('style');
+style.innerHTML = [
+	'html {height: 100%;}',
+	'body {',
+		'min-height: 100%;',
+		'display: flex;',
+		'justify-content: center;',
+		'align-items: center;',
+		'text-align: center;',
+	'}',
+	'[data-hook=drop-zone] {',
+		'background-color: #ddd;',
+		'min-height: 100px;',
+		'padding: 10px;',
+		'margin-bottom: 20px;',
+	'}',
+	'[data-hook=drop-zone].document-hover {',
+		'background-color: #c8e4e4;',
+	'}',
+	'[data-hook=drop-zone].file-holder-hover {',
+		'background-color: #c8ffc8;',
+	'}',
+	'[data-hook=drop-zone] [data-hook=label] {',
+		'line-height: 100px;',
+	'}',
+	'[data-hook=drop-zone].has-files [data-hook=label] {',
+		'display: none;',
+	'}'
+].join('');
+
+document.body.appendChild(style);
